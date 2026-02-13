d67347e40b291f2f26102c6230db23c9639ae9b4
Endy Utomo <endyutomo234@gmail.com>
2026-02-13 07:27:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4789078318-6099c",
+  "appId": "1:682220976505:web:df1209f7d9334c54ea97a8",
+  "apiKey": "AIzaSyCQcsc-HyZdCiqnzN5meICrSykjYVemLGU",
+  "authDomain": "studio-4789078318-6099c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "682220976505"
+};
